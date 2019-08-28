c889dfe9e52a30c6921bcdaf0e32a15f3849142d
Alex Higuita <Johan.A.Higuita.-ND@disney.com>
2019-08-28 16:36:19 -0500

    car schema updated, mapped all properties

--- src/schema/index.js
+++ src/schema/index.js
@@ -19,7 +19,26 @@ const serviceController = require('../controller/Services');
 // Define Object Types
 const carType = new GraphQLObjectType({
     name: 'Car',
-    fields: ()=> ({})
+    fields: ()=> ({
+        _id: {type: GraphQLID},
+        title: {type: GraphQLString},
+        brand: {type: GraphQLString},
+        price: {type: GraphQLString},
+        age: {type: GraphQLInt},
+        owner_id: {type: GraphQLID},
+        owner: {
+            type: ownerType,
+            async resolve(parent, args){
+                return await ownerController.getSingleOwner({id: parent.owner._id})
+            }
+        },
+        services: {
+            type: serviceType,
+            async resolve(parent, args){
+                return await serviceController.getCarsService({id: parent._id})
+            }
+        }
+    })
 })
 
 const ownerType = new GraphQLObjectType({
